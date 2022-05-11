cf73d204a1f8640eb1582130f6cb1c00541bda63
Yi Lin <85797292+yilinnnnn@users.noreply.github.com>
2022-05-11 22:28:37 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,17 +9,15 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Jobably Test',
+  domain: 'jobably.it',
+  author: 'Yi Lin',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  linkedin: 'Jobably',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
